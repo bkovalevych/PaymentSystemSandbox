8d1995f31688b9469d06d67db19111262fa928f7
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-05-10 21:23:47 +0300

    Implemented service for regular user

---
+++ PaymentSystemSandbox/Pages/RegularUser/YourPaymentTransactions.cshtml.cs
@@ -0,0 +1,87 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PaymentSystemSandbox.Data;
+using PaymentSystemSandbox.Data.Entities;
+using PaymentSystemSandbox.Models;
+using PaymentSystemSandbox.Services.Interfaces;
+
+namespace PaymentSystemSandbox.Pages.RegularUser
+{
+    public class YourPaymentTransactionsModel : PageModel
+    {
+        private readonly PaymentSystemSandbox.Data.ApplicationDbContext _context;
+        private readonly IUserPaymentTransactionService _transactionService;
+
+        public YourPaymentTransactionsModel(ApplicationDbContext context, IUserPaymentTransactionService transactionService)
+        {
+            _context = context;
+            _transactionService = transactionService;
+        }
+
+        public PagingList<PaymentTransaction> PaymentTransaction { get;set; }
+
+        public string NextDisabled { get; set; } = "";
+        public int NextTopIndex { get; set; } = 0;
+
+        public string PrevDisabled { get; set; } = "";
+        public int PrevTopIndex { get; set; } = 0;
+        
+        [BindProperty]
+        public string PaymentType { get; set; }
+
+        public List<string> PaymentTypes => new List<string>()
+        {
+            "All types",
+            "From me",
+            "To me"
+        };
+
+        public async Task OnGetAsync(int? top, int? offset, string paymentType)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return;
+            }
+            var list = new SelectList(new List<string>()
+            {
+                "All types",
+                "From me",
+                "To me"
+            });
+            PaymentType = paymentType;
+            if (list.FirstOrDefault(it => it.Value == PaymentType) is SelectListItem it)
+            {
+                it.Selected = true;
+            };
+            ViewData["PaymentTypes"] = list; 
+            Expression<Func<PaymentTransaction, bool>> filter = PaymentType switch
+            {
+                "From me" => it => it.FromWallet.UserId == userId,
+                "To me" => it => it.ToWallet.UserId == userId,
+                _ => _ => true,
+            };
+            PaymentTransaction = await _transactionService.GetTransactionsByUserAsync(userId, top, offset, filter);
+            if (PaymentTransaction.Top + PaymentTransaction.Offset > PaymentTransaction.TotalCount)
+            {
+                NextDisabled = "disabled";
+            }
+            if (PaymentTransaction.Top == 0)
+            {
+                PrevDisabled = "disabled";
+            }
+
+            NextTopIndex = PaymentTransaction.Top + PaymentTransaction.Offset;
+            PrevTopIndex = Math.Max(0, PaymentTransaction.Top - PaymentTransaction.Offset);
+        }
+    }
+}
---
+++ PaymentSystemSandbox/Services/UserPaymentTransactionService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaymentSystemSandbox.Data;
+using PaymentSystemSandbox.Data.Entities;
+using PaymentSystemSandbox.Models;
+using PaymentSystemSandbox.Services.Interfaces;
+using System.Linq.Expressions;
+
+namespace PaymentSystemSandbox.Services
+{
+    public class UserPaymentTransactionService : IUserPaymentTransactionService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserPaymentTransactionService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PagingList<PaymentTransaction>> GetTransactionsByUserAsync(string userId, int? top = 0, int? offset = 20, Expression<Func<PaymentTransaction, bool>> filter = null)
+        {
+            var result = new PagingList<PaymentTransaction>()
+            {
+                Top = top ?? 0,
+                Offset = offset ?? 20,
+            };
+            var userPayments = _context.PaymentTransactions
+                .Where(it => it.FromWallet.UserId == userId || it.ToWallet.UserId == userId);
+            result.TotalCount = await userPayments.CountAsync();
+            result.AddRange(await userPayments
+                .Include(it => it.FromWallet)
+                    .ThenInclude(it => it.User)
+                .Include(it => it.ToWallet)
+                    .ThenInclude(it => it.User)
+                .Where(filter ?? (_ => true))
+                .Skip(result.Top)
+                .Take(result.Offset).ToListAsync());
+
+            return result;
+        }
+
+        public PagingList<PaymentTransaction> GetTransactionsByUser(string userId, int? top, int? offset, Expression<Func<PaymentTransaction, bool>> filter)
+        {
+            var result = new PagingList<PaymentTransaction>()
+            {
+                Top = top ?? 0,
+                Offset = offset ?? 20,
+            };
+            var userPayments = _context.PaymentTransactions
+                .Where(it => it.FromWallet.UserId == userId || it.ToWallet.UserId == userId);
+            result.TotalCount = userPayments.Count();
+            result.AddRange(userPayments
+                .Include(it => it.FromWallet)
+                    .ThenInclude(it => it.User)
+                .Include(it => it.ToWallet)
+                    .ThenInclude(it => it.User)
+                .Where(filter ?? (_ => true))
+                .Skip(result.Top)
+                .Take(result.Offset));
+
+            return result;
+        }
+    }
+}
